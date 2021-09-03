f639f7f14f0d662b85c42996fad314ead3077be9
RobinIR <robinanr.kuri@gmail.com>
2021-09-03 20:08:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyNode from './components/node.components';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNode />
+    </div>
+  );
+}
+
+export default App;
